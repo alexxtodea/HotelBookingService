242dc0e1119caee2355b2ca13b2278e1b308430c
alexxtodea <103114853+alexxtodea@users.noreply.github.com>
2022-10-27 16:09:53 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+ 
+ 
+ <router-view/>
+</template>
+
+<script>
+import RoomsComponent from './components/RoomsComponent.vue';
+
+
+export default {
+  name: 'Rooms',
+  components: {
+    RoomsComponent
+    
+}}
+
+</script>
+
+<style scoped>
+ 
+ * {
+  margin: 0px;
+  padding:0px;
+}
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import Rooms from '../views/RoomsView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Room from '../views/RoomView.vue'
+
+const routes = [
+
+  {
+    path: '/rooms/',
+    name: 'rooms',
+    component: Rooms,
+    
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    //component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: `/find/:Id`,
+    name: 'room',
+    component: Room,
+    props: true
+  }
+
+
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
